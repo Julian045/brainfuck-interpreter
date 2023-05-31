2e7d172a7370b6ab09f4adcb8e718db485d26fc2
Julian <22413046+Julian045@users.noreply.github.com>
2023-05-31 15:31:00 +0200

    Create BrainfuckInterpreter.js
    Replace the bfCode variable with your own Brainfuck code and execute

---
+++ BrainfuckInterpreter.js
@@ -0,0 +1,83 @@
+class BrainfuckInterpreter {
+  constructor() {
+    this.memory = new Uint8Array(30000); // Brainfuck memory
+    this.pointer = 0; // Memory pointer
+    this.inputBuffer = ''; // Input buffer
+    this.outputBuffer = ''; // Output buffer
+  }
+
+  interpret(code, input = '') {
+    this.reset();
+    this.inputBuffer = input;
+    let output = '';
+
+    for (let i = 0; i < code.length; i++) {
+      const command = code.charAt(i);
+
+      switch (command) {
+        case '>':
+          this.pointer++;
+          break;
+        case '<':
+          this.pointer--;
+          break;
+        case '+':
+          this.memory[this.pointer]++;
+          break;
+        case '-':
+          this.memory[this.pointer]--;
+          break;
+        case '.':
+          output += String.fromCharCode(this.memory[this.pointer]);
+          break;
+        case ',':
+          if (this.inputBuffer.length === 0) {
+            throw new Error('Input buffer is empty!');
+          }
+          this.memory[this.pointer] = this.inputBuffer.charCodeAt(0);
+          this.inputBuffer = this.inputBuffer.slice(1);
+          break;
+        case '[':
+          if (this.memory[this.pointer] === 0) {
+            let loopCount = 1;
+            while (loopCount > 0) {
+              i++;
+              const c = code.charAt(i);
+              if (c === '[') loopCount++;
+              else if (c === ']') loopCount--;
+            }
+          }
+          break;
+        case ']':
+          if (this.memory[this.pointer] !== 0) {
+            let loopCount = 1;
+            while (loopCount > 0) {
+              i--;
+              const c = code.charAt(i);
+              if (c === ']') loopCount++;
+              else if (c === '[') loopCount--;
+            }
+          }
+          break;
+        default:
+          // Ignore any non-Brainfuck characters
+          break;
+      }
+    }
+
+    return output;
+  }
+
+  reset() {
+    this.memory.fill(0);
+    this.pointer = 0;
+    this.inputBuffer = '';
+    this.outputBuffer = '';
+  }
+}
+
+// Example usage
+const bfCode = '++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.'; // Brainfuck code
+const interpreter = new BrainfuckInterpreter();
+const output = interpreter.interpret(bfCode);
+console.log(output); //Hello World
